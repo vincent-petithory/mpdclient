7fd586c7ae6d1ddbef363d996e6af22dadff5d41
Vincent Petithory <vincent.petithory@gmail.com>
2013-09-16 01:18:06 +0200

    Add LGPL v3 license and headers

--- subscription.go
+++ subscription.go
@@ -1,3 +1,22 @@
+/* Copyright (C) 2013 Vincent Petithory <vincent.petithory@gmail.com>
+ *
+ * This file is part of mpdclient.
+ *
+ * mpdclient is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * mpdclient is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with mpdclient.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package mpdclient
 
 import (
